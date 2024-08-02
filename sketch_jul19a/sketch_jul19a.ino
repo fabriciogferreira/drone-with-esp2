14048fb3df28bb1695d96c3898e36ced68348630
Fabrício Gonçalves Ferreira <fgoncalves@olie.ai>
2024-08-02 15:59:46 -0300

    feat: get range of joysticks

--- sketch_jul19a/sketch_jul19a.ino
+++ sketch_jul19a/sketch_jul19a.ino
@@ -3,11 +3,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-template<typename T, int N>
-int getArraySize(T (&array)[N]) {
-  return N;
-}
-
 //-------------------------------------|DISPLAY|-------------------------------------
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -17,65 +12,105 @@ int getArraySize(T (&array)[N]) {
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 Display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 //-------------------------------------|CONTROL|-------------------------------------
-//---|??(J1s), ??(J2s)
-const int JOYSTICK_BUTTON_PINS[] = {25, 26};
-//---|Yaw(J1x), Roll(J2x), Pitch(J2y)
 //DOF = Degrees Of Freedom
-const int DOF_ROTATIONS_PINS[] = {35, 33, 32};
-//---|Throttle(J1y)
-const int THROTTLE_PIN = {34};
+//J1x, J1y, J1z, J2x, J2y, J2z
+const int PIN_YAW = 35;
+const int PIN_THROTTLE = 34;
+const int PIN_J1Z = 25;
+const int PIN_ROLL = 33;
+const int PIN_PITCH = 32;
+const int PIN_J2Z = 26;
+
+const int *PT_PINS_XY_JS[] = {&PIN_YAW, &PIN_THROTTLE, &PIN_ROLL, &PIN_PITCH};
+int maxRangeOfJoystickAxes[] = {0, 0, 0, 0};
+unsigned int minRangeOfJoystickAxes[] = {UINT_MAX, UINT_MAX, UINT_MAX, UINT_MAX};
 
 void startSerial(){
-  Serial.begin(115200);  
+  Serial.begin(115200);
 }
 
 void configDisplay(){
   Display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  Display.setCursor(0,0);
   Display.setTextSize(1);
   Display.setTextColor(SSD1306_WHITE);
+  Display.clearDisplay();
 }
 
-void printOnDisplay(String text){
-  Display.clearDisplay();
-  Display.setCursor(0,0);
+void printOnDisplay(String text, bool resetCursor = NULL, int delayTime = 0, bool clearDisplay = NULL){
+  if (clearDisplay) Display.clearDisplay();
+  if (resetCursor) Display.setCursor(0,0);
   Display.println(text);
   Display.display();
-  delay(500);
+  if (delayTime) delay(delayTime);
+}
+
+template<typename T, int N>
+int getArraySize(T (&array)[N]) {
+  return N;
 }
 
 void configJoystick(){
-  printOnDisplay("Configurando joysticks");
-  for (int i = 0; i < getArraySize(DOF_ROTATIONS_PINS); i++) {
-    pinMode(DOF_ROTATIONS_PINS[i], INPUT);
+  printOnDisplay("Configurando joysticks", true, 1000, true);
+  for (int i = 0; i < getArraySize(PT_PINS_XY_JS); i++) {
+    pinMode(*PT_PINS_XY_JS[i], INPUT);
   }
 
-  for (int i = 0; i < getArraySize(JOYSTICK_BUTTON_PINS); i++) {
-    pinMode(JOYSTICK_BUTTON_PINS[i], INPUT_PULLUP);
-  }
+  pinMode(PIN_J1Z, INPUT_PULLUP);
+  pinMode(PIN_J2Z, INPUT_PULLUP);
+}
 
-  pinMode(THROTTLE_PIN, INPUT);
+void stopUntilPressToContinue(String text){
+  printOnDisplay(text);
+  
+  while(digitalRead(PIN_J2Z));
+  while(!digitalRead(PIN_J2Z));
+}
+
+void getRangeOfJoystickAxes(){
+  printOnDisplay("Mova os joysticks em todas as direções", true, 0, true);
+
+  stopUntilPressToContinue("Aperte R3 para prosseguir");
+
+  int value = 0;
+
+  do{
+    Display.clearDisplay();
+    Display.setCursor(0,0);
+
+    for (int i = 0; i < getArraySize(PT_PINS_XY_JS); i++) {
+      value = analogRead(*PT_PINS_XY_JS[i]);
+      if(value > maxRangeOfJoystickAxes[i]){
+        maxRangeOfJoystickAxes[i] = value;
+      } else if (value < minRangeOfJoystickAxes[i]) {
+        minRangeOfJoystickAxes[i] = value;
+      }
+
+      printOnDisplay(String(*PT_PINS_XY_JS[i]) + ": " + String(minRangeOfJoystickAxes[i]) + " - " + String(maxRangeOfJoystickAxes[i]));
+    }
+  }while(digitalRead(PIN_J2Z));
 }
 
 void setup() {
   startSerial();
   configDisplay();
   configJoystick();
+  getRangeOfJoystickAxes();
 }
 
 void loop() {
-  for (int i = 0; i < getArraySize(DOF_ROTATIONS_PINS); i++) {
-    Serial.print(analogRead(DOF_ROTATIONS_PINS[i]));
+  for (int i = 0; i < getArraySize(PT_PINS_XY_JS); i++) {
+    Serial.print(analogRead(*PT_PINS_XY_JS[i]));
     Serial.print("\t");
   }
 
-  Serial.print(analogRead(THROTTLE_PIN));
+  Serial.print(digitalRead(PIN_J1Z));
   Serial.print("\t");
 
-  for (int i = 0; i < getArraySize(JOYSTICK_BUTTON_PINS); i++) {
-    Serial.print(!digitalRead(JOYSTICK_BUTTON_PINS[i]));
-    Serial.print("\t");
-  }
+  Serial.print(digitalRead(PIN_J2Z));
+  Serial.print("\t");
 
   Serial.println();
 }
