c97e6755a42c2f31a581ed2ef7294762de56609b
Fabrício Gonçalves Ferreira <fgoncalves@olie.ai>
2024-08-03 07:55:01 -0300

    feat: start esp now

--- Códigos/NewDrone/NewDrone.ino
+++ Códigos/NewDrone/NewDrone.ino
@@ -1,12 +1,20 @@
 //------------------------------------|LIBRARIES|------------------------------------
 #include <WiFi.h>
+#include <esp_now.h>
 
 void StartStationMode() {
   WiFi.mode(WIFI_STA);
 }
 
+void StartEspNow() {
+  if (!(esp_now_init() == ESP_OK)) {
+    ESP.restart();
+  }
+}
+
 void setup() {
   StartStationMode();
+  StartEspNow();
 }
 
 void loop() {
